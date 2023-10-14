090658bdadfe70c513c6a5a30bb9eb00fe6031a9
paolap1998 <paolaf0980@gmail.com>
2023-10-14 12:58:26 -0400

    Added play button to main menu that brings user to gameplay scene

---
+++ Assets/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    /* Allows for easier access to all hte scenes available in the game and we can add and remove as needed,
+     * 
+     * Note that the order and names need to match the build settings scenes list though!
+     */
+    public enum Scene
+    {
+        MainMenu,
+        GamePlay,
+        GameWin,
+        GameOver
+    }
+
+    /* Whenever a new scene needs to be loaded, this method can be called in any script
+     * using the GameStateManager Instance. 
+     * 
+     * Look at the MainMenuUI script, to see how that's done on the click of a button if 
+     * unsure what to do.
+     */
+    public void LoadScene(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString()); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI scripts/MainMenuUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] Button _startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startButton.onClick.AddListener(startGame);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void startGame()
+    {
+        GameStateManager.Instance.LoadScene(GameStateManager.Scene.GamePlay);
+    }
+}
