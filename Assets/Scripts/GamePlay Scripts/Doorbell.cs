c35e2b5f4e36223240bbd00bc3fa021e8de6038c
Anthony Calderone <anthony.calderone2000@yahoo.com>
2023-10-20 18:53:44 -0400

    Doorbell Script updated so now no compile errors (NEEDS TO BE CHECKED)

--- Assets/Scripts/GamePlay Scripts/Doorbell.cs
+++ Assets/Scripts/GamePlay Scripts/Doorbell.cs
@@ -96,3 +96,54 @@
 // =======
          
 // >>>>>>> parent of 75d75faf (win state)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorbell : MonoBehaviour
+{
+    private AudioSource doorBellAudio;
+    public Animator doorAnimator;
+    public Animator lookAnimator;
+
+    void Start()
+    {
+        doorBellAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Door clicked");
+        doorBellAudio.Play();
+        StartCoroutine(DoorOpen());
+    }
+
+    private IEnumerator DoorOpen()
+    {
+        yield return new WaitForSeconds(2f);
+        GameObject door = GameObject.FindGameObjectWithTag("door");
+        Animator doorAnimator = door.GetComponent<Animator>();
+
+        if (door != null)
+        {
+            doorAnimator.SetBool("Open", true);
+            Debug.Log("Door animation");
+        }
+
+        StartCoroutine(Look());
+    }
+
+    private IEnumerator Look()
+    {
+        yield return new WaitForSeconds(2f);
+        GameObject character = GameObject.FindGameObjectWithTag("character");
+        Animator lookAnimator = character.GetComponent<Animator>();
+
+        if (character != null)
+        {
+            lookAnimator.SetBool("isOpen", true);
+            Debug.Log("Look animation");
+        }
+    }
+}
